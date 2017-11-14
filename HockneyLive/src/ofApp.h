b743ce93ceeb89e61ac6f46cad8e652655a77547
Bentleyj <jgbentley10@gmail.com>
2017-11-14 19:05:02 +0000

    Adding async video player

--- HockneyLive/src/ofApp.h
+++ HockneyLive/src/ofApp.h
@@ -27,7 +27,7 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-    
+
         ofShader crossProcess;
     
         ofImage curveTexture;
