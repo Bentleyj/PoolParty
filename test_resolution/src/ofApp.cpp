3e68f05f652f616aa63ca4cfc43a253639f1013b
James <jgbentley10@gmail.com>
2018-01-30 11:17:24 +0000

    Updates on the shoot day

--- test_resolution/src/ofApp.cpp
+++ test_resolution/src/ofApp.cpp
@@ -10,17 +10,38 @@ void ofApp::setup(){
     
     gui.setup("controls", "settings/settings.xml");
     gui.add(scale.set("Scale", 1, 0, 1));
+    gui.add(yPos.set("yPos", 0, -ofGetHeight(), ofGetHeight()));
+    gui.add(boxSize.set("boxSize", 177, 100, 500));
+    gui.add(displayScale.set("scale", 1, 0.1, 10));
     gui.loadFromFile("settings.settings.xml");
+    
+    loader.clearPaths();
+    videoPaths = loader.load("videos");
+    player.load(videoPaths[0]);
+    player.setLoopState(OF_LOOP_NORMAL);
+    player.play();
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    player.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    
+    ofPushMatrix();
+    ofSetColor(255);
+    ofTranslate(0, yPos);
+    ofScale(displayScale, displayScale);
+    //player.draw(0, 0);
     img.draw(0, 0, originalSize.x, originalSize.y);
+    ofSetColor(255, 0, 0);
+    ofNoFill();
+    ofDrawRectangle(0, 0, boxSize, boxSize);
+    ofPopMatrix();
+    
     gui.draw();
 }
 
@@ -31,12 +52,20 @@ void ofApp::keyPressed(int key){
         imageIndex %= imagePaths.size();
         img.load(imagePaths[imageIndex]);
         originalSize = ofVec2f(img.getWidth(), img.getHeight());
+        videoIndex++;
+        videoIndex %= videoPaths.size();
+        player.load(videoPaths[videoIndex]);
+        player.play();
     }
     if(key == OF_KEY_LEFT) {
         imageIndex--;
         imageIndex = (imageIndex < 0) ? imagePaths.size()-1 : imageIndex;
         img.load(imagePaths[imageIndex]);
         originalSize = ofVec2f(img.getWidth(), img.getHeight());
+        videoIndex--;
+        videoIndex = (videoIndex < 0) ? videoPaths.size()-1 : videoIndex;
+        player.load(videoPaths[videoIndex]);
+        player.play();
     }
     if(key == ' ') {
         img.load(imagePaths[imageIndex]);
